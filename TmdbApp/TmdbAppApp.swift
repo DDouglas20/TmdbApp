a156b8a837ab47f3d1c37fe89b97abd62d8766a1
Ddouglas20 <DeQuan.Douglas2020@gmail.com>
2025-07-03 19:53:33 -0400

    Initial Commit

---
+++ TmdbApp/TmdbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TmdbAppApp.swift
+//  TmdbApp
+//
+//  Created by DeQuan Douglas on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TmdbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TmdbAppTests/TmdbAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TmdbAppTests.swift
+//  TmdbAppTests
+//
+//  Created by DeQuan Douglas on 7/3/25.
+//
+
+import Testing
+@testable import TmdbApp
+
+struct TmdbAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
